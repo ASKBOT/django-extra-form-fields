13c1fcdc925cef337f07834e4ec2577b8c50844f
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-09-10 06:38:25 -0300

    changed style of initializing settings

--- django_extra_form_fields/conf.py
+++ django_extra_form_fields/conf.py
@@ -12,11 +12,9 @@ from django.conf import settings as django_settings
 from multi_registry import MultiRegistry
 from django_extra_form_fields import default_settings
 
-settings = MultiRegistry()
-settings.append(django_settings)
-
+registries = [django_settings, default_settings]
 extra_settings = getattr(django_settings, 'EXTRA_SETTINGS_MODULE', None)
 if extra_settings:
-    settings.append(extra_settings)
+    registries.insert(1, extra_settings)
 
-settings.append(default_settings)
+settings = MultiRegistry(registries)
