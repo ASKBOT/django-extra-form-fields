b7eaca048e3dfe72feec8b337df2f3fd42e9efdf
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-09-10 08:32:42 -0300

    adapted to the new style of MultiRegistry.__init__()

--- django_extra_form_fields/conf.py
+++ django_extra_form_fields/conf.py
@@ -8,13 +8,14 @@ django settings or the extra settings module:
 * USERNAME_REGEX_STRING - regex for acceptable user name
 * EMAIL_UNIQUE - boolean - require unique email address sitewide
 """
-from django.conf import settings as django_settings
+from django.conf.settings import django_settings
 from multi_registry import MultiRegistry
-from django_extra_form_fields import default_settings
 
-registries = [django_settings, default_settings]
+settings = MultiRegistry(
+    django_settings,
+    'django_extra_form_fields.default_settings'
+)
 extra_settings = getattr(django_settings, 'EXTRA_SETTINGS_MODULE', None)
 if extra_settings:
-    registries.insert(1, extra_settings)
+    settings.insert(1, extra_settings)
 
-settings = MultiRegistry(registries)
